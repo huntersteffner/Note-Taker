40a4c12bc808455e2bebe1db0dded09460ef3192
huntersteffner <huntersteffner@gmail.com>
2022-07-19 18:50:43 -0400

    Links to static html pages

--- server.js
+++ server.js
@@ -1,19 +1,33 @@
 const express = require('express')
 const path = require('path')
-const api = require('./db/db.json')
-
+const notes = require('./db/db.json')
 const app = express()
 
-// Get all members
-app.get('/api/members', (req, res) => {
-    res.json(api)
-})
-// Get single member
-app.get('api/members/:id', (req, res) => {
-    res.json(api.filter(api => api.id === parseInt(req.params.id)))
-})
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 
-app.use(express.static(path.join(__dirname, 'public')))
+// Get all notes
+app.get('/api/notes', (req, res) => {
+    res.json(notes)
+})
+// // Get single member
+// app.get('api/notes/:id', (req, res) => {
+//     const found = notes.some(note => note.id === parseInt(req.params.id))
+
+//     if(found) {
+
+//         res.json(notes.filter(note => note.id === parseInt(req.params.id)))
+//     } else {
+//         res.status(400).json({msg: `No results for ${req.params.id}`})
+//     }
+
+// })
+
+app.use(express.static('public'))
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+})
 
 const PORT = process.env.PORT || 5000
 
