cd219d9c8a758233a2cbb681e2e2ad002306e0d1
huntersteffner <huntersteffner@gmail.com>
2022-07-23 13:19:45 -0400

    Refactored code

--- server.js
+++ server.js
@@ -5,12 +5,10 @@ const path = require('path')
 const notes = require('./db/db.json')
 const { v4: uuidv4 } = require('uuid');
 const app = express()
-// const api = require('./public/assets/js/index')
-
-// const notesStringed = JSON.stringify(notes)
-
+// Middleware
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+// Function to read and append to JSON database
 const readFileAsync = util.promisify(fs.readFile)
 const readAndAppend = (content, file) => {
     fs.readFile(file, 'utf8', (err, data) => {
@@ -23,76 +21,46 @@ const readAndAppend = (content, file) => {
       }
     });
   };
-
+// function for writing new information to JSON database
   const writeToFile = (destination, content) =>
   fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
     err ? console.error(err) : console.info(`\nData written to ${destination}`)
   );
-
-  // const readFromFile = util.promisify(fs.readFile);
-
 // Get all notes
 app.get('/api/notes', (req, res) => {
   let database = readFileAsync('./db/db.json', 'utf8')
   .then( (database) => {
-
     database = JSON.parse(database)
       res.json(notes)
   }
-
   )
 })
 // // Get single note
 app.get('/api/notes/:id', (req, res) => {
-    // const found = notes.some(note => note.id === parseInt(req.params.id))
-    // // console.log(found)
-
-    // if(found) {
-
-    //     res.json(notes.filter(note => note.id === parseInt(req.params.id)))
-    // } else {
-    //     res.status(400).json({msg: `No results for ${req.params.id}`})
-    // }
-
-    let idDisplay = ''
-
     const noteId = req.params.id
-    console.log(noteId)
-    console.log(notes)
-    
-
     for(let i = 0; i < notes.length; i++) {
-      console.log(notes[i].id)
       if(noteId == notes[i].id) {
         res.json(notes[i])
        } 
     }
-
-
-
 })
-
+// Function for posting new note
 app.post('/api/notes', (req, res) => {
-    console.log(req.body);
     const randomNumber = uuidv4()
-  
     const { title, text } = req.body;
-  
     if (req.body) {
       const newNote = {
         id: randomNumber,
         title,
         text,
       };
-  
       readAndAppend(newNote, './db/db.json');
       res.json(`Note added successfully 🚀`);
     } else {
       res.error('Error in adding note');
     }
-    
   });
-
+// Function for deleting a note
   app.delete('/api/notes/:id', (req, res) => {
     const found = notes.some(note => note.id === req.params.id)
     if(!found) {
@@ -104,16 +72,11 @@ app.post('/api/notes', (req, res) => {
       })
       res.json(notes)
     }
-    
   })
-
-
+// Linking html pages to different URL paths
 app.use(express.static('public'))
-
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 })
-
 const PORT = process.env.PORT || 5000
-
 app.listen(PORT, () => console.log(`Server has started on port ${PORT}`))
